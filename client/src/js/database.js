fca9d13e5c73977962e3b50ec9ba8c1c17c1b845
Vivid Quill <sjdoublec@gmail.com>
2023-11-01 13:19:21 -0500

    Added logic to 'database.js' for working with IndexedDB.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method that accepts some content and adds it to the database:
+export const putDb = async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction('jate', 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.put({ id: 1, value: content });
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const result = await request;
+  console.log('Data saved to database', result.value);
+};
+
+// Method that gets all the content from the database:
+export const getDb = async () => {
+  const txtEditorDB = await openDB('jate', 'readonly');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
+};
 
 initdb();
